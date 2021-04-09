7f485bb064bedac2a8c7cc8310471f20c8bcae0c
Swedemon <zergcollision@gmail.com>
2021-04-09 19:00:27 -0400

    Moved to C Arc folder.

--- src/main/java/Parameters.java
+++ src/main/java/Parameters.java
@@ -1,7 +1,5 @@
-import java.io.PrintStream;
-
 public class Parameters {
-    public static String Gw2EIExe = "C:\\Games\\MzFightReporter\\GW2EI\\GuildWars2EliteInsights.exe";
+    public static String Gw2EIExe = "C:\\Arc\\MzFightReporter\\GW2EI\\GuildWars2EliteInsights.exe";
     public static String logFolder = System.getenv("USERPROFILE") + "\\Documents\\Guild Wars 2\\addons\\arcdps\\arcdps.cbtlogs\\1\\";
     public static String thumbnail = "https://i.imgur.com/KKddNgl.png";
     public static String token = "ODI4ODU4MzM3NDYyMDU5MDE4.YGvsew.SPuwZeO_rlZg4jpPCUWrxxGyWh4";
