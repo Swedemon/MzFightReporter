6fb04cff47ebedd9f12f19b71ebe89e416866fce
Swedemon <zergcollision@gmail.com>
2021-04-10 00:47:27 -0400

    Minor param tweak.

--- src/main/java/Parameters.java
+++ src/main/java/Parameters.java
@@ -1,6 +1,8 @@
 public class Parameters {
     public static String Gw2EIExe = "C:\\Arc\\MzFightReporter\\GW2EI\\GuildWars2EliteInsights.exe";
-    public static String logFolder = System.getenv("USERPROFILE") + "\\Documents\\Guild Wars 2\\addons\\arcdps\\arcdps.cbtlogs\\1\\";
+    public static String logFolder =
+            //"C:\\Arc\\arcdps.cbtlogs\\WvW\\Elite Apes\\";
+            System.getenv("USERPROFILE") + "\\Documents\\Guild Wars 2\\addons\\arcdps\\arcdps.cbtlogs\\1\\";
     public static String thumbnail = "https://i.imgur.com/KKddNgl.png";
     public static String token = "ODI4ODU4MzM3NDYyMDU5MDE4.YGvsew.SPuwZeO_rlZg4jpPCUWrxxGyWh4";
     public static String discordChannel = "fight-reports-test";
